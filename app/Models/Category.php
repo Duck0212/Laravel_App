7a56a078c3f09853ce582d6f99795d5986f50ce5
Duck0212 <tranhoangduc0212@gmail.com>
2026-04-03 16:24:02 +0700

    bai6

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,6 +9,8 @@ class Category extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name', 'desc'];
+
     public function products()
     {
         return $this->hasMany(Product::class);
